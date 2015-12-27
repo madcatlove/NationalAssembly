e279e63cec2762f47e38ea63cab4e3d9c9cc6a10
Song, Nuri <noorisong@gmail.com>
2015-12-27 22:35:17 +0900

    Utils 에 globalfont 함수 추가완료/ Activity 에서 부르는 부분은 수정 안함

--- app/src/main/java/kr/or/osan21/nationalassembly/Utils/CustomFont.java
+++ app/src/main/java/kr/or/osan21/nationalassembly/Utils/CustomFont.java
@@ -2,6 +2,9 @@ package kr.or.osan21.nationalassembly.Utils;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class CustomFont {
 
@@ -10,6 +13,16 @@ public class CustomFont {
 
         Typeface tf = Typeface.createFromAsset(ctx.getAssets(), fontName);
         return tf;
+    }
+
+    public static void setGlobalFont(Typeface mytf, ViewGroup root) {
+        for (int i = 0; i < root.getChildCount(); i++) {
+            View child = root.getChildAt(i);
+            if (child instanceof TextView)
+                ((TextView)child).setTypeface(mytf);
+            else if (child instanceof ViewGroup)
+                setGlobalFont(mytf, (ViewGroup)child);
+        }
     }
 
 }
