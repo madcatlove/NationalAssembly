ed7074344c038621f61ced0cd8ceb10cb84045ec
madcat <kr.madcat@gmail.com>
2015-12-30 22:49:36 +0900

    media api

---
+++ app/src/main/java/kr/or/osan21/nationalassembly/Media/Media.java
@@ -0,0 +1,54 @@
+package kr.or.osan21.nationalassembly.Media;
+
+/**
+ * Created by madcat on 12/30/15.
+ */
+public class Media {
+
+    private Integer num;
+    private String title;
+    private String content;
+    private String regdate;
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(String regdate) {
+        this.regdate = regdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Media{" +
+                "num=" + num +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", regdate='" + regdate + '\'' +
+                '}';
+    }
+}
