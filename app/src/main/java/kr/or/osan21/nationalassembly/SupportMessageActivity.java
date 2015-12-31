cc8904567b4a0857e829498afe5a314ddc89c8fc
Jeonghoon Cho <jeonghoon1107@gmail.com>
2015-12-31 20:46:18 +0900

    SupportMessage list 완료

--- app/src/main/java/kr/or/osan21/nationalassembly/SupportMessageActivity.java
+++ app/src/main/java/kr/or/osan21/nationalassembly/SupportMessageActivity.java
@@ -6,18 +6,14 @@ import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,8 +27,7 @@ public class SupportMessageActivity extends AppCompatActivity {
     private final String LOG_TAG = "SupportMessageActivity";
     private SupportMessageAPI supportMessageAPI;
     private ListView support_message_list;
-    private RecyclerView rv;
-    private RecyclerView.Adapter RVadapter;
+    private CustomAdapter adapter;
     private Button write;
 
     @Override
@@ -43,10 +38,29 @@ public class SupportMessageActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(Color.BLACK);
         }
 
+        support_message_list = (ListView) findViewById(R.id.support_message_list);
+        support_message_list.setDivider(null);
+        adapter = new CustomAdapter();
+        support_message_list.setAdapter(adapter);
+
         supportMessageAPI = new SupportMessageAPI();
 
+        supportMessageAPI.getMessageList(new Callback<List<SupportMessage>>() {
+            @Override
+            public void success(List<SupportMessage> supportMessages, Response response) {
+                Log.d(LOG_TAG, "리스트 받기");
+                adapter.setMessageItems(supportMessages);
+                adapter.notifyDataSetInvalidated();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+
         //건의사항 및 격려 작성하러 가기
-        write = (Button)findViewById(R.id.support_message_write);
+        write = (Button) findViewById(R.id.support_message_write);
         write.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,6 +68,74 @@ public class SupportMessageActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    public class CustomAdapter extends BaseAdapter {
+
+
+        // 문자열을 보관 할 ArrayList
+        private List<SupportMessage> messageItems;
+
+        // 생성자
+        public CustomAdapter() {
+            messageItems = new ArrayList<SupportMessage>();
+        }
+
+        public void setMessageItems(List<SupportMessage> messageItems) {
+            this.messageItems = messageItems;
+        }
+
+        @Override
+        public int getCount() {
+            return messageItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return messageItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        // 출력 될 아이템 관리
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final int pos = position;
+            final Context context = parent.getContext();
+
+            // 리스트가 길어지면서 현재 화면에 보이지 않는 아이템은 converView가 null인 상태로 들어 옴
+            if (convertView == null) {
+                // view가 null일 경우 커스텀 레이아웃을 얻어 옴
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.message_item_layout, parent, false);
+
+                TextView title = (TextView) convertView.findViewById(R.id.support_message_title);
+                title.setText(messageItems.get(position).getTitle());
+
+                TextView username = (TextView) convertView.findViewById(R.id.support_message_username);
+                username.setText(messageItems.get(position).getUsername());
+
+                //TextView replyCnt = (TextView) convertView.findViewById(R.id.support_message_reply);
+                //replyCnt.setText(messageItems.get(position).getReply_count());
+
+                TextView content = (TextView) convertView.findViewById(R.id.support_message_content);
+                content.setText(messageItems.get(position).getContent());
+
+                // 리스트 아이템을 터치 했을 때 이벤트 발생
+                convertView.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getBaseContext(), SupportMessageContentActivity.class).putExtra("m_id", messageItems.get(pos).getNum()));
+                    }
+                });
+            }
+            return convertView;
+        }
 
     }
 }
---
+++ app/src/main/java/kr/or/osan21/nationalassembly/SupportMessageContentActivity.java
@@ -0,0 +1,13 @@
+package kr.or.osan21.nationalassembly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SupportMessageContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_support_message_content);
+    }
+}
