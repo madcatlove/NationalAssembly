230424868fd7f10bc4bd1f9bee16783684bdd171
madcat <kr.madcat@gmail.com>
2016-01-01 00:55:41 +0900

    폰트캐시 도입

--- app/src/main/java/kr/or/osan21/nationalassembly/Utils/CustomFont.java
+++ app/src/main/java/kr/or/osan21/nationalassembly/Utils/CustomFont.java
@@ -2,13 +2,24 @@ package kr.or.osan21.nationalassembly.Utils;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 public class CustomFont {
 
+    private static final String LOG_TAG = "CustomFont";
+
+    /* 캐시 저장소에서 폰트를 가져옴 */
     public static Typeface getCustomFont(Context ctx, String name) {
+        return Cache.getFont(ctx,name);
+    }
+
+    /* 폰트 생성 */
+    private static Typeface createFont(Context ctx, String name) {
         String fontName = "SourceHanSansKR-Regular.otf";
 
         if(name.equalsIgnoreCase("CJKM"))
@@ -35,6 +46,26 @@ public class CustomFont {
                 ((TextView)child).setTypeface(mytf);
             else if (child instanceof ViewGroup)
                 setGlobalFont(mytf, (ViewGroup)child);
+        }
+    }
+
+
+    /* 폰트 캐싱을 위함 */
+    public static class Cache {
+        private static HashMap<String, Typeface> data = new HashMap<String,Typeface>();
+
+        public static Typeface getFont(Context ctx, String name) {
+
+            if( data.containsKey(name) ) {
+                Log.d(LOG_TAG, "FontCache hit!. use");
+                return data.get(name);
+            }
+
+            Log.d(LOG_TAG, "FontCache miss!. create!");
+            Typeface tf = createFont(ctx, name);
+            data.put(name, tf);
+
+            return tf;
         }
     }
 
