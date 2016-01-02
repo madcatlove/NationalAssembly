e8dc3ed9316d987c5f421d26ad2b30f180ddb085
madcat <kr.madcat@gmail.com>
2016-01-03 00:38:00 +0900

    추후에 API주소 변경할것

--- app/src/main/java/kr/or/osan21/nationalassembly/Utils/API.java
+++ app/src/main/java/kr/or/osan21/nationalassembly/Utils/API.java
@@ -7,9 +7,16 @@ import retrofit.RestAdapter;
  */
 public class API  {
 
-    public static final String API_URL = "http://assembly.my.n-pure.net/index.php/";
+    // TESTING
+    // http://assembly.my.n-pure.net/
+    public static final String API_URL = "http://assembly.my.n-pure.net/http://assembly.my.n-pure.net/index.php/";
     public static final String STATIC_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/static/";
-    public static final String UPLOAD_URL = "http://assembly.my.n-pure.net/upload/";
+    public static final String UPLOAD_URL = "http://assembly.my.n-pure.net/http://assembly.my.n-pure.net/upload/";
+
+    // PRODUCTION
+//    public static final String API_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/index.php/";
+//    public static final String STATIC_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/static/";
+//    public static final String UPLOAD_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/upload/";
 
     protected RestAdapter mRestAdapter;
 
