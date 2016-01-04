dc5f4b69d8393ed567df5882e27e5ed7591416d4
Song, Nuri <noorisong@gmail.com>
2016-01-05 00:30:58 +0900

    오산비전 콘텐르 수정완료!

--- app/src/main/java/kr/or/osan21/nationalassembly/VisionFiveThreeActivity.java
+++ app/src/main/java/kr/or/osan21/nationalassembly/VisionFiveThreeActivity.java
@@ -1,11 +1,17 @@
 package kr.or.osan21.nationalassembly;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import kr.or.osan21.nationalassembly.Utils.CustomFont;
 
@@ -17,13 +23,31 @@ public class VisionFiveThreeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_vision_five_three);
 
         TextView title = (TextView)findViewById(R.id.vision_five_three_title);
-        TextView content = (TextView)findViewById(R.id.vision_five_three_content);
-
         title.setTypeface(CustomFont.getCustomFont(this, "CJKB"));
-        content.setTypeface(CustomFont.getCustomFont(this, "CJKR"));
 
         TextView bar_title = (TextView)findViewById(R.id.myImageViewText);
         bar_title.setTypeface(CustomFont.getCustomFont(this, "hans"));
+
+        //이미지 적용 (본문내용+이미지)
+        ImageView img = (ImageView)findViewById(R.id.vision_five_three_img);
+        ImageView content = (ImageView)findViewById(R.id.vision_five_three_content);
+
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+
+        final int width = display.getWidth();
+        final int height = display.getHeight()/3;
+
+        Glide.with(this)
+                .load(R.drawable.vision_five_three)
+                .override(width, height)
+                .centerCrop()
+                .into(img);
+
+        Glide.with(this)
+                .load(R.drawable.vision_five_content_03)
+                .fitCenter()
+                .into(content);
 
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(Color.BLACK);
