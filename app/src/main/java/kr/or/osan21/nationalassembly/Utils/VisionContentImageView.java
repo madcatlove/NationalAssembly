2b2766492db90fe1e6d2a56f484b39407c4e5b05
madcat <kr.madcat@gmail.com>
2016-02-03 06:38:18 +0900

    view

--- app/src/main/java/kr/or/osan21/nationalassembly/Utils/VisionContentImageView.java
+++ app/src/main/java/kr/or/osan21/nationalassembly/Utils/VisionContentImageView.java
@@ -1,7 +1,74 @@
 package kr.or.osan21.nationalassembly.Utils;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
 /**
  * Created by madcat on 2/3/16.
  */
-public class VisionContentImageView {
+public class VisionContentImageView extends ImageView {
+
+    private static final String LOG_TAG ="VisionImageView";
+    private int width;
+    private int height;
+    private ViewReady viewReady = null;
+    private boolean done = false;
+
+    public interface ViewReady {
+        public void onViewReady(View v, int width, int height);
+    }
+
+    public VisionContentImageView(Context context) {
+        super(context);
+    }
+
+    public VisionContentImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VisionContentImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        width = MeasureSpec.getSize(widthMeasureSpec);
+        height = MeasureSpec.getSize(heightMeasureSpec);
+        Log.d(LOG_TAG, "onMeasure " + String.format("w:%d, h:%d", width, height));
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if( !done) {
+            if( viewReady != null) {
+                viewReady.onViewReady(this, width, height);
+                done = true;
+            }
+        }
+    }
+
+
+    public int getViewWidth() {
+        return width;
+    }
+
+
+    public int getViewHeight() {
+        return height;
+    }
+
+    public void setViewReady(ViewReady vr) {
+        viewReady = vr;
+        done = false;
+    }
 }
--- app/src/main/java/kr/or/osan21/nationalassembly/VisionContentActivity.java
+++ app/src/main/java/kr/or/osan21/nationalassembly/VisionContentActivity.java
@@ -7,10 +7,12 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -19,13 +21,15 @@ import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
 
 import kr.or.osan21.nationalassembly.Utils.CustomFont;
+import kr.or.osan21.nationalassembly.Utils.VisionContentImageView;
 
 public class VisionContentActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "VisionContentActivity";
 
     Bitmap bitmap;
-    private ImageView content;
+    private VisionContentImageView content;
+    private int imageId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,31 +42,31 @@ public class VisionContentActivity extends AppCompatActivity {
         TextView bar_title = (TextView) findViewById(R.id.vision_content_bar);
         bar_title.setTypeface(CustomFont.getCustomFont(this, "hans"));
 
-        Integer id = getIntent().getIntExtra("v_id",1);
+        Integer id = getIntent().getIntExtra("v_id", 1);
         //타이틀 적용
-        String titleTxt="";
-        if(id == 31)
+        String titleTxt = "";
+        if (id == 31)
             titleTxt = "오산 시민회관 재건축,\n복합문화체육센터로";
-        else if(id == 32)
+        else if (id == 32)
             titleTxt = "경기 남부 광역 교통의 중심,\n오산역 환승센터";
-        else if(id == 33)
+        else if (id == 33)
             titleTxt = "동탄(KTX역) ~ 세교\n복선 전철 예비타당성 확정";
-        else if(id == 51)
+        else if (id == 51)
             titleTxt = "독산성 복원으로 유네스코\n세계문화유산 확대";
-        else if(id == 52)
+        else if (id == 52)
             titleTxt = "UN초전 평화공원,\n전 세계인이 찾는\n평화의 상징으로";
-        else if(id == 53)
+        else if (id == 53)
             titleTxt = "미니어처 테마파크\n아시아 최대 소인국 테마파크";
-        else if(id == 54)
+        else if (id == 54)
             titleTxt = "아이들이 멱감고,\n야생화 피는 오산천";
-        else if(id == 55)
+        else if (id == 55)
             titleTxt = "오색시장, 오매장터\n문화관광지구로 변신";
 
         title.setText(titleTxt);
 
         //이미지 적용 (본문내용+이미지)
         final ImageView img = (ImageView) findViewById(R.id.vision_content_img);
-        content = (ImageView) findViewById(R.id.vision_content_text);
+        content = (VisionContentImageView) findViewById(R.id.vision_content_text);
 
         WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
@@ -71,21 +75,21 @@ public class VisionContentActivity extends AppCompatActivity {
         final int height = display.getHeight() / 3;
 
         Integer imgResource = null;
-        if(id == 31)
+        if (id == 31)
             imgResource = R.drawable.vision_three_one;
-        if(id == 32)
+        if (id == 32)
             imgResource = R.drawable.vision_three_two;
-        else if(id == 33)
+        else if (id == 33)
             imgResource = R.drawable.vision_three_three;
-        else if(id == 51)
+        else if (id == 51)
             imgResource = R.drawable.vision_five_one;
-        else if(id == 52)
+        else if (id == 52)
             imgResource = R.drawable.vision_five_two;
-        else if(id == 53)
+        else if (id == 53)
             imgResource = R.drawable.vision_five_three;
-        else if(id == 54)
+        else if (id == 54)
             imgResource = R.drawable.vision_five_four;
-        else if(id == 55)
+        else if (id == 55)
             imgResource = R.drawable.vision_five_five;
 
         Glide.with(this)
@@ -109,68 +113,87 @@ public class VisionContentActivity extends AppCompatActivity {
                 .into(img);
         */
 
-        int imageId = 0;
-        if(id == 31) {
+        imageId = 0;
+        if (id == 31) {
             //imgResource = R.drawable.vision_three_content_01;
             //imgResource = R.drawable.t_01;
 //            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.t_01);
             imageId = R.drawable.t_01;
         }
-        if(id == 32) {
+        if (id == 32) {
             //imgResource = R.drawable.vision_three_content_02;
             //imgResource = R.drawable.t_02;
 //            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.t_02);
             imageId = R.drawable.t_02;
-        }
-        else if(id == 33) {
+        } else if (id == 33) {
             //imgResource = R.drawable.vision_three_content_03;
             //imgResource = R.drawable.t_03;
 //            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.t_03);
             imageId = R.drawable.t_03;
-        }
-        else if(id == 51) {
+        } else if (id == 51) {
             //imgResource = R.drawable.vision_five_content_01;
             //imgResource = R.drawable.f_01;
 //            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.f_01);
             imageId = R.drawable.f_01;
-        }
-        else if(id == 52) {
+        } else if (id == 52) {
             //imgResource = R.drawable.vision_five_content_02;
             //imgResource = R.drawable.f_02;
 //            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.f_02);
             imageId = R.drawable.f_02;
-        }
-        else if(id == 53) {
+        } else if (id == 53) {
             //imgResource = R.drawable.vision_five_content_03;
             //imgResource = R.drawable.f_03;
 //            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.f_03);
             imageId = R.drawable.f_03;
-        }
-        else if(id == 54) {
+        } else if (id == 54) {
             //imgResource = R.drawable.vision_five_content_04;
             //imgResource = R.drawable.f_04;
 //            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.f_04);
             imageId = R.drawable.f_04;
-        }
-        else if(id == 55) {
+        } else if (id == 55) {
             //imgResource = R.drawable.vision_five_content_05;
             //imgResource = R.drawable.f_05;
 //            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.f_05);
             imageId = R.drawable.f_05;
         }
 
-        Glide.with(this)
-                .load(imageId)
-                .asBitmap()
-                .fitCenter()
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(new SimpleTarget<Bitmap>() {
-                    @Override
-                    public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
-                        content.setImageBitmap(resource);
+        Log.i(LOG_TAG, " Display width : " + wm.getDefaultDisplay().getWidth());
 
-                    }
-                });
+        content.setViewReady(new VisionContentImageView.ViewReady() {
+            @Override
+            public void onViewReady(View v, int width, int height) {
+                Glide.with(VisionContentActivity.this)
+                        .load(imageId)
+                        .asBitmap()
+                        .fitCenter()
+                        .into(new SimpleTarget<Bitmap>() {
+                            @Override
+                            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                                int w = resource.getWidth();
+                                int h = resource.getHeight();
+                                Log.i(LOG_TAG, String.format("width:%d, height:%d", w, h));
+
+                                int viewWidth = content.getViewWidth();
+                                int viewHeight = content.getViewHeight();
+
+                                Log.i(LOG_TAG, String.format("content view W:%d, H:%d", viewWidth, viewHeight));
+
+                                float ratio = (float) viewWidth / w;
+                                int newHeight = (int) Math.round(ratio * h);
+
+                                Log.i(LOG_TAG, String.format("scaledWidth: %d, scaledHeight :%d", viewWidth, newHeight));
+
+                                Bitmap scaled = Bitmap.createScaledBitmap(resource, viewWidth, newHeight, false);
+                                content.setLayoutParams(new LinearLayout.LayoutParams(viewWidth, newHeight));
+
+                                content.setImageBitmap(scaled);
+                            }
+                        });
+            }
+        });
+
+        content.invalidate();
+
 
 //        content.setImageBitmap(bitmap);
 //        content.setImageResource(imageId);
