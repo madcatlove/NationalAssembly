197c3f7851f10177338a56dfa095cd7da37edada
madcat <kr.madcat@gmail.com>
2016-02-05 09:05:17 +0900

    API 서버 주소 변경

--- app/src/main/java/kr/or/osan21/nationalassembly/Utils/API.java
+++ app/src/main/java/kr/or/osan21/nationalassembly/Utils/API.java
@@ -9,15 +9,15 @@ public class API  {
 
     // TESTING
     // http://assembly.my.n-pure.net/
-    public static final String API_URL = "http://assembly.my.n-pure.net/index.php/";
-    public static final String STATIC_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/static/";
-    public static final String UPLOAD_URL = "http://assembly.my.n-pure.net/upload/";
+//    public static final String API_URL = "http://assembly.my.n-pure.net/index.php/";
+//    public static final String STATIC_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/static/";
+//    public static final String UPLOAD_URL = "http://assembly.my.n-pure.net/upload/";
     public static final String YOUTUBE_DEV_KEY = "AIzaSyA1PhfQKbQKnriHu4fo4vZxnQkGBPDn8sE";
 
     // PRODUCTION
-//    public static final String API_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/index.php/";
-//    public static final String STATIC_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/static/";
-//    public static final String UPLOAD_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/upload/";
+    public static final String API_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/index.php/";
+    public static final String STATIC_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/static/";
+    public static final String UPLOAD_URL = "http://ec2-52-192-93-201.ap-northeast-1.compute.amazonaws.com/upload/";
 
     protected RestAdapter mRestAdapter;
 
